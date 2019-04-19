8028c3af3c60063a915a0594a54d08db7b76fddf
JohnKoba2 <48258179+JohnKoba2@users.noreply.github.com>
2019-04-19 18:36:00 -0400

    Created a deck
    Added code to create a deck to the game. YAY!

--- BlackJackSim/BlackJackSim/BlackJack.cpp
+++ BlackJackSim/BlackJackSim/BlackJack.cpp
@@ -4,14 +4,83 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
-
-
+#include <fstream>
 using namespace std;
 
-int t = 0, fgam = 00, sgam = 00, tgam = 00, lgam = 00;
-int scor = 0, fgwon = 0, sgwon = 0, tgwon = 0, lgwon = 0, quit = 0;
-int fwalet = 0000, swalet = 0000, twalet = 0000, lwalet = 0000;
+const char* Heart = "H";
+const char* Club = "C";
+const char* Diamond = "D";
+const char* Spade = "S";
+
+const char* rankName[] = {
+"","A","2","3","4","5","6","7","8","9","10","J","Q","K","A"
+};
+
+CARD standardDeck[] = { 
+	{Two, Heart},
+	{Three, Heart},
+	{Four, Heart},
+	{Five, Heart},
+	{Six, Heart },
+	{Seven, Heart },
+	{Eight, Heart },
+	{Nine, Heart },
+	{Ten, Heart },
+	{Jack, Heart },
+	{Queen, Heart },
+	{King, Heart },
+	{Ace, Heart },
+	{Two, Club },
+	{Three, Club },
+	{Four, Club },
+	{Five, Club },
+	{Six, Club },
+	{Seven, Club },
+	{Eight, Club },
+	{Nine, Club },
+	{Ten, Club },
+	{Jack, Club },
+	{Queen, Club },
+	{King, Club },
+	{Ace, Club },
+	{Two, Diamond },
+	{Three, Diamond },
+	{Four, Diamond },
+	{Five, Diamond },
+	{Six, Diamond },
+	{Seven, Diamond },
+	{Eight, Diamond },
+	{Nine, Diamond },
+	{Ten, Diamond },
+	{Jack, Diamond },
+	{Queen, Diamond },
+	{King, Diamond },
+	{Ace, Diamond },
+	{Two, Spade },
+	{Three, Spade },
+	{Four, Spade },
+	{Five, Spade },
+	{Six, Spade },
+	{Seven, Spade },
+	{Eight, Spade },
+	{Nine, Spade },
+	{Ten, Spade },
+	{Jack, Spade },
+	{Queen, Spade },
+	{King, Spade },
+	{Ace, Spade },
+};
+
+int games = 0, fgam = 00, sgam = 00, tgam = 00, lgam = 00;
+int gwon = 0, fgwon = 0, sgwon = 0, tgwon = 0, lgwon = 0, quit = 0;
+int fwalet = 0000, swalet = 0000, twalet = 0000, lwalet = 0000, walet =0;
 string name, fnam = "AAA", snam = "AAA", tnam = "AAA", lnam = "AAA";
+ifstream playerRecord;
+
+#define DSIZE (sizeof(standardDeck) / sizeof(CARD))
+int DeckSize = DSIZE;
+CARD* shoe[DSIZE];
+
 
 
 
@@ -35,9 +104,9 @@ int main()
 			<< "2222222222222222222222         1111111111111111111111111\n"
 			<< "2222222222222222222222         1111111111111111111111111\n"
 			<< endl << endl << endl << endl
-			<< "Press P to Play"
-			<< "Press L for Leaderboard"
-			<< "Press H for Help"
+			<< "Press P to Play\n"
+			<< "Press L for Leaderboard\n"
+			<< "Press H for Help\n"
 			<< "Press Q to Quit.";
 
 		cin >> Ans;
@@ -46,7 +115,9 @@ int main()
 		{
 		case 'P':
 			//Begin the game
-			beginGame();
+			//beginGame();
+			shuffle();
+			cout << rankName[shoe[1]->rank] << " , " << shoe[1]->Suit << endl << shoe[2]->rank << endl << shoe[3]->rank << endl;
 			break;
 		case 'L':
 			//Display the leaderboard
@@ -67,7 +138,7 @@ void beginGame()
 {
 	char ans;
 	bool game = true;
-	cout << "To start a New game press N  or to load a game press L, Press Q to quit or H for help.";
+	cout << "To start a New game press N \n or to load a game press L, \n Press Q to quit or H for help.";
 	cin >> ans;
 	ans = toupper(ans);
 	while (game)
@@ -115,9 +186,73 @@ void showLeaderboard()
 	return;
 }
 
+void updateLeaderboard() 
+{
+	 // Accepts your initials then moves your score up to the proper position, pushing down other scores if you beat them. 
+		cout << "What are your Initials?";
+		cin >> name;
+		if (gwon > lgwon)
+		{
+
+			lnam = name;
+			lgam = games;
+			lgwon = gwon;
+			lwalet = walet;
+
+			if (gwon  > tgwon)
+			{
+				lnam = tnam;
+				lgam = tgam;
+				lgwon = tgwon;
+				lwalet = twalet;
+
+				tnam = name;
+				tgam = games;
+				tgwon = gwon;
+				twalet = walet;
+
+				if (gwon > sgwon)
+				{
+
+					tnam = snam;
+					tgwon = sgwon;
+					tgam = sgam;
+					twalet = swalet;
+
+					snam = name;
+					sgam = games;
+					sgwon = gwon;
+					swalet = walet;
+
+					if (gwon > fgwon)
+					{
+
+						snam = fnam;
+						sgam = fgam;
+						sgwon = fgwon;
+						swalet = fwalet;
+
+						fnam = name;
+						fgam = games;
+						fgwon = gwon;
+						fwalet = walet;
+					}
+				}
+			}
+		}
+		else
+			cout << "Sorry you didnt score high enough to make it on the Leaderboard. ";
+		showLeaderboard();
+			return;
+	
+	}
+
+
 void clearScreen()
 {
-	//prints 100 new lines?
+	int n;
+	for (n = 0; n < 10; n++)
+		printf("\n\n\n\n\n\n\n\n\n\n");
 
 }
 
@@ -140,3 +275,26 @@ void showHelp()
 		<< "Once you Stand the dealer will reveal their second card and hit or stand up until they reach 18 or bust. Then the higher hand will win the bet.\n"
 		<< "In the case of a tie, a push will occur and you will get your initial bet back. ";
 }		
+
+
+
+
+
+void deckCreation() {
+	for (int ind = 0; ind < DeckSize; ind++){
+		shoe[ind] = &standardDeck[ind];
+	}
+}
+
+void shuffle() {
+	deckCreation();
+	for (int n = DeckSize-1; n > 0; --n) {
+		int r = rand() % n;
+
+		CARD* cardSwap = shoe[r];
+		shoe[r] = shoe[n];
+		shoe[n] = cardSwap;
+	}
+
+} 
+
--- BlackJackSim/BlackJackSim/pch.h
+++ BlackJackSim/BlackJackSim/pch.h
@@ -9,9 +9,38 @@
 #ifndef PCH_H
 #define PCH_H
 
+//#define HEART "H"
+//#define CLUB "C"
+//#define DIAMOND "D"
+//#define SPADE "S"
+
+typedef enum {
+Two = 2,
+Three = 3,
+Four = 4,
+Five = 5,
+Six = 6,
+Seven = 7,
+Eight = 8,
+Nine = 9,
+Ten = 10,
+Jack = 11,
+Queen = 12,
+King = 13,
+Ace = 14
+} RANK;
+
+
+typedef struct Card {
+	RANK rank;
+	const char* Suit;
+}CARD;
+
+
 void showHelp();
 void showLeaderboard();
 void clearScreen();
-
+void shuffle();
+void beginGame();
 
 #endif //PCH_H
